f77f84548d11aa5138029d3802d790e97a93d5b3
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2015-11-25 01:27:56 +0900

    refactoring

--- src/extension.ts
+++ src/extension.ts
@@ -87,9 +87,14 @@ export function activate(ctx: vscode.ExtensionContext): void {
     var output = vscode.window.createOutputChannel('Runner: ' + action + ' ' + fileName);
     output.show(vscode.ViewColumn.Two);
     editor.show()
+	// TODO parse line and spawn command without shells. because it's not
+	// possible to get an error code of execute on windows.
     var sh = win32 ? 'cmd' : '/bin/sh';
-    var shflag = win32 ? '/s /c' : '-c';
-    var args = document.isDirty || document.isUntitled ? [shflag, action] : [shflag, action + ' ' + fileName];
+    var fromInput = document.isDirty || document.isUntitled;
+    var args = win32 ?
+      (fromInput ? ['/s', '/c', action] : ['/s', '/c', action + ' ' + fileName])
+    :
+      (fromInput ? ['-c', action] : ['-c', action + ' ' + fileName]);
     var opts = { cwd: cwd, detached: false };
     if (win32) opts['windowsVerbatimArguments'] = true;
     var child = cp.spawn(sh, args, opts);
@@ -108,7 +113,7 @@ export function activate(ctx: vscode.ExtensionContext): void {
       else if (code)
         output.appendLine('Exited with status ' + code)
     });
-    if (document.isDirty || document.isUntitled) {
+    if (fromInput) {
       child.stdin.write(document.getText());
     }
     child.stdin.end();
